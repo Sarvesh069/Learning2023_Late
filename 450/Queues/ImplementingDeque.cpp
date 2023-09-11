3ca8568ff3ced40ed7b79f41993e39a2c3f6a595
Nerdyjerk69 <sarveshdasgupta@gmail.com>
2023-09-11 17:47:20 +0530

    Added Javascript

---
+++ 450/Queues/ImplementingDeque.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Dequee{
+    public:
+    int *arr;
+    int front = 0, back = -1;
+
+    void pushBack(int data){
+        
+
+    }
+
+    void pushFront(int data){
+
+    }
+
+    int popFront(){
+
+    }
+
+    int popBack(){
+
+    }
+
+    int frontElement(){
+
+
+    }
+
+    int backElement(){
+
+    }
+
+    bool isEmpty(){
+
+    }
+
+};
+
+
+int main(){
+    return 0;
+}
---
+++ Javascript/day1/Variables.js
@@ -0,0 +1,19 @@
+console.log("Variables")
+
+let a = 89
+var test = 119
+console.log(a)
+a = "harry"
+console.log(a) //Dynamic change of data type
+
+let b = 9
+// b is identifier
+let B = 10
+//Case sensitive
+console.log(b)
+console.log(B)
+
+// let a = 8; Doesnt let redeclared
+a = 9
+var test = 121 //Allows redeclaration
+console.log(a)
